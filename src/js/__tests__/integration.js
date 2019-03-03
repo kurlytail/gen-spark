8638403a937f8d1ebe6aac464fd1635ba81f382b
ashwin <ashwinkn@hotmail.com>
2019-03-03 15:17:27 -0600

    generated

---
+++ src/js/__tests__/integration.js
@@ -0,0 +1,31 @@
+import { execSync } from 'child_process';
+
+describe('# integration test', () => {
+    beforeEach(() => {
+        execSync('rm -rf testoutput');
+    });
+
+    it('## should print help options', () => {
+        const output = execSync('./scripts/sgen-spark.sh -h').toString();
+        expect(output).toMatchSnapshot();
+    });
+
+    it('## should generate design', () => {
+        const output = execSync('./scripts/sgen-spark.sh -d src/test/fixture/design.json -o testoutput').toString();
+        expect(output).toMatchSnapshot();
+    });
+
+    it('## should generate design with merge', () => {
+        let output = execSync('./scripts/sgen-spark.sh -d src/test/fixture/design.json -o testoutput').toString();
+        expect(output).toMatchSnapshot();
+        output = execSync('./scripts/sgen-spark.sh -d src/test/fixture/design.json -o testoutput').toString();
+        expect(output).toMatchSnapshot();
+    });
+
+    it('## should generate design and run spark commands', () => {
+        let output = execSync('./scripts/sgen-spark.sh -d src/test/fixture/design.json -o testoutput').toString();
+        expect(output).toMatchSnapshot();
+        output = execSync('npm install', { cwd: 'testoutput' }).toString();
+        output = execSync('npm run lint', { cwd: 'testoutput' }).toString();
+    });
+});
